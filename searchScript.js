1058c65f18ca9b6644d83d1b8b89eb0065893f2a
matt russell <mrussell@cs.tufts.edu>
2021-02-13 07:49:23 -0500

    mvp backend communication complete

--- searchScript.js
+++ searchScript.js
@@ -1,13 +1,10 @@
-// this will be generated using database requests eventually, but is hardcoded
-
-// for the purposes of this mvp TODO add database functionality!
-var image_paths = ["images/like.png","images/love.png","images/laugh.png"];
+emotions =  ["_128544", "_128546","_128552","_128562",
+            "_128578", "_128525", "_128514", "_128526", "_129320"];
 
 //get array of search result divs
 var resultDivs = [...document.getElementsByClassName("g")]; //convert to array
-console.log(resultDivs);
-//takes the div containing a search result link and scrapes the link
-// from it
+
+// takes the div containing a search result link and scrapes the url
 function getLink(div) {
     var children = div.children;
     var index = 0;
@@ -36,55 +33,26 @@ function getLink(div) {
 
 }
 
-//send a POST request to the server, asking for three image paths
-// and maybe other data in the future.
-var getReactsFunc = function(url)
-{
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST","https://the-prism.herokuapp.com/getReacts", true);
-    xhr.setRequestHeader("Content-type","application/json");
-    xhr.onreadystatechange = function (e) {
-        if (xhr.readyState == 4) {
-           if (xhr.status === 200) {
-                console.log("Response:", xhr.responseText);
-            }
-            else{
-                console.log("Something broke.")
-                console.error(xhr.statusText);
-            }
-        }
-    }
-    xhr.send(JSON.stringify({ 'url': url})); 
-}
-
-
-// for each result div, make a corresponding area to put emojis
-// if it is a regular result type
-while (resultDivs.length != 0) {
-    div = resultDivs.shift();
-    if (div.classList.length == 1) {
-        //get links to each google result page
-        var url = getLink(div);
-
-        //get react data
-        getReactsFunc(url);
-
-        //make the corresponding emojis
-        makeEmojis(image_paths, div);
-    }
-}
-
 
 // make a div that contains 1-3 image parameters (string paths) specified as arguments
 // note - if less than three are provided, only those provided should be used.
 // otherwise, the first three images will be used
-// IMAGE GETTING IS STILL TODO, ALSO NEED TO HANDLE LESS INPUT
-function makeEmojis(paths, emojiArea) {
+// NEED TO HANDLE LESS INPUT
+function makeEmojis(emojis, emojiArea) {    
     emojiArea.setAttribute('class',"g emojiArea");
-
-    let emo_value = 128511;
+    console.log(emojis);
     for (let i = 0; i < 3; i++) {
-        emo_value += 1;
+        
+        let num_emotes, emo_value;
+        
+        num_emotes = emojis[i][1];
+
+        if (num_emotes == 0){
+            emo_value = "129297";
+        }else{
+            emo_value = emojis[i][0].substring(1);    //remove underscore.
+        }
+
         var emojiDiv = document.createElement("div");
         const emoji  = document.createElement('span');
         emoji.innerHTML = "&#" + emo_value.toString() + ";";
@@ -92,5 +60,41 @@ function makeEmojis(paths, emojiArea) {
         emojiArea.appendChild(emojiDiv);
         emojiDiv.setAttribute('class', "emoji num" + ( i + 1 ));
 	    emojiDiv.style.zIndex = "0";
+    }
+}
+
+//send a POST request to the server, asking for three image paths
+// and maybe other data in the future.
+async function getReactsFunc(div)
+{
+    var url = getLink(div);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST","https://the-prism.herokuapp.com/getReacts", true);
+    xhr.setRequestHeader("Content-type","application/json");
+    xhr.onreadystatechange = function (e) {
+        if (xhr.readyState == 4) {
+           if (xhr.status === 200) {            
+                response = JSON.parse(xhr.response);                 
+                if (!Object.keys(response).includes("response")){
+                    makeEmojis(JSON.parse(xhr.response), div);                               
+                }
+            }
+            else{
+                console.log("Something broke.");
+                console.error(xhr.statusText);
+                return("something broke.");
+            }
+        }
+    }
+    xhr.send(JSON.stringify( { 'url': url } )); 
+}
+
+// for each result div, make a corresponding area to put emojis
+// if it is a regular result type
+while (resultDivs.length != 0) {
+    div = resultDivs.shift();
+    if (div.classList.length == 1) {
+        getReactsFunc(div);
     }
 }
